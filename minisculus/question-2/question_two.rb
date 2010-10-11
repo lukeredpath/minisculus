3d85897368a6be6a937218659a56936eda59b488
Luke Redpath <contact@lukeredpath.co.uk>
2010-10-11 17:46:58 +0100

    A failing acceptance spec

---
+++ minisculus/question-2/question_two.rb
@@ -0,0 +1,7 @@
+module Minisculus
+  module QuestionTwo
+    class MarkII
+      
+    end
+  end
+end
--- spec/question_two_spec.rb
+++ spec/question_two_spec.rb
@@ -1,2 +1,10 @@
 require 'spec_helper'
+require 'question-2/question_two'
 
+describe "Mark II example with wheels set to 2 and 5" do
+  include Minisculus::QuestionTwo
+  
+  it "should translate 'abc' to 'STU'" do
+    MarkII.new(2, 5).decrypt('abc').should == 'STU'
+  end
+end
